32b011a36f5c81a3d04f48a82c1f3ff6afccbc9c
Corey Powell <mistdragon100@gmail.com>
2020-03-01 13:55:42 -0500

    Removed dead test deps

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-require 'codeclimate-test-reporter'
-require 'simplecov'
-
 def fixture_pathname(name)
   File.expand_path(File.join('fixtures', name), __dir__)
 end
-
-CodeClimate::TestReporter.start
-SimpleCov.start
