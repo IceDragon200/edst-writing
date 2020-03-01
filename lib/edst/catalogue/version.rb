790cc412b9417618e742e06a855ec3abcfc2f47c
Corey Powell <mistdragon100@gmail.com>
2020-03-01 13:53:59 -0500

    Bumped version to 1.0.0

--- lib/edst/catalogue/version.rb
+++ lib/edst/catalogue/version.rb
@@ -2,7 +2,7 @@ module EDST
   module Catalogue
     # Le Version module, what more do you need to know.
     module Version
-      MAJOR, MINOR, TEENY, PATCH = 0, 3, 1, nil
+      MAJOR, MINOR, TEENY, PATCH = 1, 0, 0, nil
       STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
     end
     VERSION = Version::STRING
